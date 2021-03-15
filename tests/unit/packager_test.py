c38f0304b7ebe4168c44b557efaee245bd77fc21
babbleshack <dcrl94@gmail.com>
2021-03-15 19:23:56 +0000

    Clean up testing code

--- tests/unit/packager_test.py
+++ tests/unit/packager_test.py
@@ -1,5 +1,6 @@
 import os
 from kraft.package.package import Packager
+from kraft.package.package import ImageWrapper
 from kraft.package.oci.opencontainers.digest import SHA256 
 from .. import unittest
 
@@ -16,15 +17,27 @@ EXPECTED_HASH_VALUE = "c1ac6403f2519d86296264021abc18255b5e83c389172a58c819c9107
 
 class PackagerTest(unittest.TestCase):
     def test_create_oci_image(self):
+
+        #TODO add uk_conf to image
+        image = ImageWrapper(
+            path="./tests/artifacts/image",
+            architecture="x86_64",
+            platform="kvm",
+            uk_conf="./tests/artifacts/image"
+        )
         packager = Packager(
-            image="./tests/artifacts/image",
-            uk_conf="./tests/artifacts/image",
+            image=image,
             digest_algorithm=SHA256
         )
         content = packager.create_oci_filesystem()
+        self.content = content
         print(content)
         self.assertEqual(EXPECTED_HASH_VALUE, content['digest'].encoded())
         self.assertTrue(os.path.isfile(content['path']))
+
+    # TODO delete temp dir
+    #def tearDown(self)
+    #    os.rmdir(self.content['path']) 
 
 if __name__ == '__main__':
     unittest.main()
